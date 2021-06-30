4c39df17169a05ba183c6a2d8baa1218ed6beb02
Ecstatic7 <nima.nilchian@gmail.com>
2021-06-30 22:13:31 +0330

    added new bullet icon

--- model/Bullet.cpp
+++ model/Bullet.cpp
@@ -3,8 +3,8 @@
 
 Bullet::Bullet(QGraphicsScene * scene) : QObject() , QGraphicsPixmapItem()
 {
-    setPixmap(QPixmap(":/images/bullet.png"));
-    setPos(SpaceShip::getInstance()->x()+60, SpaceShip::getInstance()->y() - 35);
+    setPixmap(QPixmap(":/images/bullet2.png"));
+    setPos(SpaceShip::getInstance()->x()+70, SpaceShip::getInstance()->y() - 35);
     scene->addItem(this);
 
     connect(bulletTimer, SIGNAL(timeout()), this, SLOT(move()));
